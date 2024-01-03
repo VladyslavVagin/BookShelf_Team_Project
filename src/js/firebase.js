e270f6113acc3dc5af2e6f910259999f94124232
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-01-03 22:09:59 +0100

    update firebase

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,13 +1,14 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDGGs_M72U47x_8OPp1fXlulwAc_R9sWFw",
-  authDomain: "bookshelf-vladyslav-pr.firebaseapp.com",
-  databaseURL: "https://bookshelf-vladyslav-pr-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "bookshelf-vladyslav-pr",
-  storageBucket: "bookshelf-vladyslav-pr.appspot.com",
-  messagingSenderId: "486061422566",
-  appId: "1:486061422566:web:65c06a043c706983c1c827",
+  apiKey: "AIzaSyBO9cjVBOhaLawHt-cSmC-d-k4SEv7vhoE",
+  authDomain: "books-shelf-12team.firebaseapp.com",
+  databaseURL:  "https://books-shelf-12team-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "books-shelf-12team",
+  storageBucket: "books-shelf-12team.appspot.com",
+  messagingSenderId: "8408632477",
+  appId: "1:8408632477:web:37da13216b9fc869039874",
+  measurementId: "G-4LLZCMXJK5"
 };
   // Initialize Firebase
  export const app = initializeApp(firebaseConfig);
